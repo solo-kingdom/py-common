8bfceafa8973b668feb9c5473c51c19e1d3ac71f
sunzhenkai <zhenkai.sun@qq.com>
2021-11-29 23:36:04 +0800

    enhance log

--- common/log.py
+++ common/log.py
@@ -11,21 +11,26 @@ LOG_K_FILE_NUMBER = 'log_file_number'
 
 class ILogger:
     def __init__(self, cfg=None):
+        self.cfg = cfg
+        self.init(logging.INFO)
+
+    def init(self, lv):
         self.logger = logging.getLogger()
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(lv)
         fmt = logging.Formatter('[%(asctime)s] %(levelname)s %(message)s',
                                 '%Y-%m-%d %H:%M:%S')
+
         console_handler = logging.StreamHandler(stream=sys.stdout)
         console_handler.setFormatter(fmt)
-        console_handler.setLevel(logging.INFO)
+        console_handler.setLevel(lv)
         self.logger.addHandler(console_handler)
 
-        if cfg:
-            file_handler = logging.handlers.RotatingFileHandler(cfg[LOG_K_PATH],
-                                                                maxBytes=cfg[LOG_K_SIZE],
-                                                                backupCount=cfg[LOG_K_FILE_NUMBER])
+        if self.cfg:
+            file_handler = logging.handlers.RotatingFileHandler(self.cfg[LOG_K_PATH],
+                                                                maxBytes=self.cfg[LOG_K_SIZE],
+                                                                backupCount=self.cfg[LOG_K_FILE_NUMBER])
             file_handler.setFormatter(fmt)
-            file_handler.setLevel(logging.INFO)
+            file_handler.setLevel(lv)
             self.logger.addHandler(file_handler)
 
     def debug(self, msg, *args, **kwargs):
@@ -39,3 +44,9 @@ class ILogger:
 
     def error(self, msg, *args, **kwargs):
         self.logger.error(msg, *args, **kwargs)
+
+    def level(self, lv):
+        self.logger.setLevel(lv)
+        for handler in self.logger.handlers:
+            if isinstance(handler, type(logging.StreamHandler())):
+                handler.setLevel(lv)
