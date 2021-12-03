d2b33410bf23210e866ebe2e9948e40a0d058b03
sunzhenkai <zhenkai.sun@qq.com>
2021-12-03 14:08:37 +0800

    fix test_file

--- request/test_file.py
+++ request/test_file.py
@@ -5,9 +5,8 @@ from common.request.file import upload
 
 class Test(TestCase):
     def test_upload(self):
-        res = upload('http://127.0.0.1:8088/api/v1/package', {
+        assert upload('http://127.0.0.1:8088/api/v1/package', {
             'space': 'global',
             'product': 'test',
             'version': '0.0.1'
         }, {}, '/Users/wii/.vimrc')
-        assert res.status_code == 200
